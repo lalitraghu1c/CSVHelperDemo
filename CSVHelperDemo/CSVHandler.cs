1aabd28429ebfff2d69d67bcbb1b3442feffc436
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-12-12 13:17:30 +0530

    [Lalit] Add: CSV Helper Demo

---
+++ CSVHelperDemo/CSVHandler.cs
@@ -0,0 +1,41 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVHelperDemo
+{
+    public class CSVHandler
+    {
+        public static void ImplementCSVHandling()
+        {
+            string importFilePath = @"D:\Projects-Bridgelabz\Learning\CSVHelper\address.csv";
+            string exportFilePath = @"D:\Projects-Bridgelabz\Learning\CSVHelper\export.csv";
+            //reading CSV File
+            using (var reader = new StreamReader(importFilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddressData>().ToList();
+                Console.WriteLine("Read data successfully from the address csv");
+                foreach (AddressData addressData in records)
+                {
+                    Console.WriteLine("\t" + addressData.Name);
+                    Console.WriteLine("\t" + addressData.Email);
+                    Console.WriteLine("\t" + addressData.Phone);
+                    Console.WriteLine("\t" + addressData.Country);
+                    Console.WriteLine("\n");
+                }
+                Console.WriteLine("\n *********************** Now Reading from csv file and write to csv file ***********************");
+                //writing CSV File
+                using (var writer = new StreamWriter(exportFilePath))
+                using (var csvExport = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csvExport.WriteRecords(records);
+                }
+            }
+        }
+    }
+}
---
+++ CSVHelperDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CSVHelperDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to CSV Problem Statement");
+            CSVHandler.ImplementCSVHandling();
+        }
+    }
+}
